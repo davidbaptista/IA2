de34880e743dcd61d3b7c209a637781e754d6515
daviderino <david.baptista00@hotmail.com>
2020-11-29 15:50:24 +0000

    ficha de redes

--- solutions/al007.py
+++ solutions/al007.py
@@ -10,43 +10,102 @@ from math import log
 
 
 def createdecisiontree(D, Y, noise = False):
-	pass
+	examples = []
+	num_attributes = len(D[0])
 
-def decisiontreelearning(examples, atributes, parent_examples):
+	for i in range(0, len(D)):
+		d = []
+		for j in range(0, num_attributes):
+			d.append(D[i][j])
+
+		examples.append(d)
+		examples.append(Y[i])
+
+	attributes = []
+
+	for j in range(0, num_attributes):
+		a = []
+		for i in range(0, len(D)):
+			a.append(D[i][j])
+		
+		attributes.append(a)
+	
+	decisiontreelearning(examples, attributes, examples)
+
+
+def decisiontreelearning(examples, attributes, parent_examples):
 	if examples is []:
 		return plurality_value(parent_examples)
+	elif same_classification(examples):
+		return examples[0][1]
+	elif attributes is []:
+		return plurality_value(examples)
 	else:
-		for i in range(0, len(atributes)):
-			imp = importance(atributes, examples)
-			if imp[i] == max(imp):
-				tree = [i, 0, 0]
+		importances = importance(attributes, examples)
+		max_importance = max(importances)
+		for i in range(0, len(importances)):
+			if importances[i] == max_importance:
+				tree = [i,] 
+				break # TODO: heuristica
 		
-		
+		i = tree[0]
+		unique_attribute_values = np.unique(np.array(attributes[i]))
+		for vk in unique_attribute_values:
+			exs = []
+			for e in examples:
+				if e[0][i] == vk:
+					exs.append(e)
+			
+			attributes.pop(i)	# attributes - A
+			subtree = decisiontreelearning(exs, attributes, examples)
+			tree.append(subtree)
 
-def plurality_value(arr):
-	counter = 0
-	num = arr[0]
+		return tree
 
-	for i in arr:
-		current = arr.count(i)
-		if(current > counter):
-			counter = current
-			num = i
+exs = [[(0,0),0], [(0,1),0], [(1,0),0], [(1,1),1]]
 
-	return num
+def same_classification(examples):
+	value = examples[0][1]
+	for i in range(1, len(examples)):
+		if value != examples[i][1]:
+			return False
+	
+	return True
+
+def plurality_value(examples):
+	n_zero = 0
+	n_ones = 0
+
+	for i in range(0, len(examples)):
+		if examples[i][1] == 0:
+			n_zero += 1
+		else:
+			n_ones += 1
+
+	# TODO: heuristica
+	if n_zero == n_ones:
+		return 0
+
+	return 0 if n_zero > n_ones else 1
+
 
 ''' list(Gain(A))
 Gain de todos os atributos [G(a1), G(a2),... G(an)]
 '''
-def importance(atributes, examples):
+def importance(attributes, examples):
 	result = []
+	p = 0
+	n = 0
 
-	p = examples.count(1)
-	n = examples.count(0)
+	for i in range(0, len(examples)):
+		if examples[i][1] == 0:
+			n += 1
+		else:
+			p += 1
 
 	B = entropy(p/(p+n))
 	
-	for attr in atributes:
+	for attr in attributes:
 		result.append(B - remainder(attr, examples, p+n))	
 	return result
 
@@ -55,32 +114,54 @@ entropy(p):
 	p: valor a calcular a entropia
 '''
 def entropy(p):
+	if p == 0 or p == 1:
+		return 0
+	
 	n = 1 - p
-	return -p * log(p, 2) + n * log(n, 2)
+	return -(p * log(p, 2) + n * log(n, 2))
 
 ''' --Remainder(A)--
 remainder(attribute_values, classifcation, pn) :-
-	attribute_values: valores da coluna do atributo
-	classification_values: valor da classificacao correspondente ao valor do atributo
+	attributes: valores da coluna do atributo
+	examples: exemplos
 	pn: p + n = numero de entradas na tabela 
 '''
-def remainder(attribute_values, classification_values, pn):
-	uniques = np.unique(attribute_values)
-	nk = 0
-	pk = 0
+def remainder(attributes, examples, pn):
+	uniques = np.unique(np.array(attributes))
 	a = []
 
 	for u in uniques:
-		for i in range(0, len(attribute_values)):
-			if attribute_values[i] == u:
-				if classification_values[i] == 0:
+		nk = 0
+		pk = 0
+		for i in range(0, len(attributes)):
+			if attributes[i] == u:
+				if examples[i][1] == 0:
 					nk += 1
 				else:
 					pk += 1
-					
+
 		a.append(((pk + nk)/pn) * entropy(pk/(pk + nk)))
+
 
 	return sum(a)
 
 def classify(T, data):
 	pass
+
+
+
+print(createdecisiontree([ [0,0], [0,1], [1,0], [1,1]], [0,0,0,1]))
+
+'''print(remainder([True, False, True, True, False, False, True, False], 
+	([(True, True), 0], 
+	[(False, False), 1],
+	[(True, True), 0], 
+	[(True, False), 0], 
+	[(False, True), 0], 
+	[(False, False), 1], 
+	[(True, False), 0], 
+	[(False, False), 0]), 8))
+'''
+
+'''print(importance([[0,0,1,1], [0,1,0,1]], 
+				[[(0,0),0], [(0,1),0], [(1,0),0], [(1,1),1]]))'''
